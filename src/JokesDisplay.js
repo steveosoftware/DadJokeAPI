8a8450d544f5073933039f2058b8c083d0fb4e65
SteveO <stinkypiece@yahoo.com>
2021-04-14 11:16:39 -0700

    initial commit

---
+++ src/JokesDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './JokesDisplay.scss';
+
+class JokesDisplay extends Component {
+  render(){
+    return (
+      <div className="JokesDisplay">
+        <h1>JokesDisplay</h1>
+      </div>
+    )
+  }
+}
+
+export default JokesDisplay;
