9ed7a9f6be7179c6a3a06505027ad4876b7bf0d8
SteveO <stinkypiece@yahoo.com>
2021-04-14 11:56:48 -0700

    add axios to fetch joke

--- src/JokesDisplay.js
+++ src/JokesDisplay.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Joke from './Joke';
 import './JokesDisplay.scss';
 
+const JOKES_API = 'https://icanhazdadjoke.com/'
+
 class JokesDisplay extends Component {
+  constructor(props){
+    super(props);
+    this.state = { fetchedJokes: [] }
+  }
+
+  async componentDidMount(){
+    const header = {
+      headers: {Accept: "application/json"}
+    }
+    const response = await axios.get(JOKES_API, header);
+    console.log(response.data.joke)
+    this.setState(st => (
+      { ...st.fetchedJokes, fetchedJokes: response.data.joke}))
+  }
+
+
   render(){
     return (
       <div className="JokesDisplay">
