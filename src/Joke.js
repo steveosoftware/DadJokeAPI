41174743bc2c1095740bc7478c1dcaf868879863
SteveO <stinkypiece@yahoo.com>
2021-04-14 20:24:10 -0700

    add button vote counter

--- src/Joke.js
+++ src/Joke.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import './JokesDisplay.scss';
 
 class Joke extends Component {
+  constructor(props){
+    super(props);
+    this.state = { vote: 0 }
+    this.vote = this.vote.bind(this);
+  }
+
+
+  vote(){
+    this.setState(st => ({ vote: st.vote + 1 }))
+  }
 
   render(){
     return (
-      <div className="Joke">{this.props.joke}</div>
+      <div className="Joke">
+        <button onClick={this.vote}>{this.state.vote}</button>
+        {this.props.joke}
+      </div>
     )
   }
 }
