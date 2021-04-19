efead3e60e934383df3e5940388f28a79a65c1c5
SteveO <stinkypiece@yahoo.com>
2021-04-19 12:28:42 -0700

    add emoji and color voting features

--- src/Joke.js
+++ src/Joke.js
@@ -4,18 +4,54 @@ import './Joke.scss';
 
 class Joke extends Component {
 
+  getColor() {
+    if (this.props.votes >= 15) {
+      return "#4CAF50";
+    } else if (this.props.votes >= 12) {
+      return "#8BC34A";
+    } else if (this.props.votes >= 9) {
+      return "#CDDC39";
+    } else if (this.props.votes >= 6) {
+      return "#FFEB3B";
+    } else if (this.props.votes >= 3) {
+      return "#FFC107";
+    } else if (this.props.votes >= 0) {
+      return "#FF9800";
+    } else {
+      return "#f44336";
+    }
+  }
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.votes >= 12) {
+      return "em em-laughing";
+    } else if (this.props.votes >= 9) {
+      return "em em-smiley";
+    } else if (this.props.votes >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.votes >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.votes >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  }
 
   render(){
     return (
       <div className="Joke">
         <div className="Joke__buttons">
         <i onClick={this.props.upvote} className="fas fa-arrow-up"></i>
-        <span className="Joke__votes">{this.props.votes}</span>
+        <span className="Joke__votes" style={{borderColor: this.getColor()}}>{this.props.votes}</span>
         <i onClick={this.props.downvote} className="fas fa-arrow-down"></i>
         </div>
 
-      <div className="Joke__text">
-      {this.props.text}
+      <div className="Joke__text">{this.props.text}</div>
+      <div className="Joke__Smiley">
+          <i className={this.getEmoji()} />
+
       </div>
 
       </div>
