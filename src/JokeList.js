ac5a4e678a5c60ac53f227952973bdcb1b076516
SteveO <stinkypiece@yahoo.com>
2021-04-19 16:41:01 -0700

    add animations

--- src/JokeList.js
+++ src/JokeList.js
@@ -94,7 +94,7 @@ async getJokes(){
           <img
           className="Sidebar__image"
           src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="icon"/>
-          <button className="Sidebar__getmore" onClick={this.handleClick}>New Jokes</button>
+          <button className="Sidebar__getmore" onClick={this.handleClick}>Fetch Jokes</button>
         </div>
 
         <div className="JokeList__jokes">
